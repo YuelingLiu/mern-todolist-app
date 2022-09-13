4ef0e9b2fb3ed3098c7cb7e526fe02ce4e5aed9e
Stella <stellaliuca@gmail.com>
2022-09-13 11:15:48 -0700

    "installed"

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+//using express.json () to get data into json format
+app.use(express.json());
+//port
+const PORT =
+  // Add port and connect to server
+
+  app.listen();
