590a87e4b3f94fee655475382c6ebc107e8a9714
Stella <stellaliuca@gmail.com>
2022-09-16 22:16:52 -0700

    :backend side

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 //using express.json () to get data into json format
 app.use(express.json());
 //port
 const PORT = process.env.PORT || 5500;
+
+//use cors
+app.use(cors());
 
 //import routes
 const TodoItemRoute = require('./routes/Todoitems');
